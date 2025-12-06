9bcff8874b021eeafddeb241e7e5b7ffb5596a10
programmeranamul <programmer.anamul@gmail.com>
2025-12-06 21:48:14 +0600

    fainal code

---
+++ src/components/MonsterStage.jsx
@@ -0,0 +1,62 @@
+import {
+    Environment,
+  MeshPortalMaterial,
+  RoundedBox,
+  Text,
+  useTexture,
+} from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import React, { useRef } from "react";
+import * as THREE from "three";
+import { easing } from "maath";
+
+function MonsterStage({
+  children,
+  texture,
+  name,
+  color,
+  active,
+  setActive,
+  hovered,
+  setHovered,
+  ...props
+}) {
+  const map = useTexture(texture);
+  const portalMaterial = useRef(null);
+useFrame((_state, delta) => {
+    const worldOpen = active === name;
+    easing.damp(portalMaterial.current, "blend", worldOpen ? 1 : 0, 0.2, delta);
+  });
+  return (
+    <group {...props}>
+      <Text
+        font="fonts/Caprasimo-Regular.ttf"
+        fontSize={0.3}
+        position={[0, -1.3, 0.051]}
+        anchorY={"bottom"}
+      >
+        {name}
+        <meshBasicMaterial color={color} toneMapped={false} />
+      </Text>
+      <RoundedBox
+        name={name}
+        args={[2, 3, 0.1]}
+        onDoubleClick={() => setActive(active === name ? null : name)}
+        onPointerEnter={() => setHovered(name)}
+        onPointerLeave={() => setHovered(null)}
+      >
+        <MeshPortalMaterial ref={portalMaterial} side={THREE.DoubleSide}>
+          <ambientLight intensity={1} />
+          <Environment preset="sunset" />
+          {children}
+          <mesh>
+            <sphereGeometry args={[5, 64, 64]} />
+            <meshStandardMaterial map={map} side={THREE.BackSide} />
+          </mesh>
+        </MeshPortalMaterial>
+      </RoundedBox>
+    </group>
+  );
+}
+
+export default MonsterStage;
